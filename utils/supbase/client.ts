1e5e395eca4e602c74e0118e1e785d61e4e4022d
Wilmer465 <153233615+Wilmer465@users.noreply.github.com>
2026-05-12 23:21:24 -0500

    main

--- utils/supbase/client.ts
+++ utils/supbase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.'https://llkfdckqovgfguponutg.supabase.co';
-const supabaseAnonKey = process.env.'sb_publishable_3LLyKJdKoG1ag4bIdDfpQg_IolLFXUQ';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
--- utils/supbase/server.ts
+++ utils/supbase/server.ts
@@ -1,8 +1,8 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.'https://llkfdckqovgfguponutg.supabase.co';
-const supabaseAnonKey = process.env.'sb_publishable_3LLyKJdKoG1ag4bIdDfpQg_IolLFXUQ';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
