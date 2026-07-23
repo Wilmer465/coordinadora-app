db3a820a21ec7c3a37972d3f8350a61f6f468820
Wilmer465 <153233615+Wilmer465@users.noreply.github.com>
2026-07-22 19:59:57 -0500

    main

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var SUPABASE_URL = 'https://llkfdckqovgfguponutg.supabase.co';
-var SUPABASE_KEY = 'sb_publishable_3LLyKJdKoG1ag4bIdDfpQg_IolLFXUQ';
+var SUPABASE_URL = 'https://tnufuoafibnvpshjwvmp.supabase.co';
+var SUPABASE_KEY = 'sb_publishable_TEHzdrdsnsPeZLCe8aNfNA_Xld24HHL';
